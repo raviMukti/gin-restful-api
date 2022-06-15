3dc9c902ff3ded2f73121e0d3cca15db4b2fcdd8
raviMukti <shinjiravi@gmail.com>
2022-06-15 12:08:59 +0700

    Implement Router Group

--- app/router.go
+++ app/router.go
@@ -1,23 +1,83 @@
 package app
 
 import (
+	"database/sql"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
+	"github.com/joho/godotenv"
 	"github.com/raviMukti/gin-restful-api/controller"
 	"github.com/raviMukti/gin-restful-api/exception"
+	"github.com/raviMukti/gin-restful-api/helper"
+	"github.com/raviMukti/gin-restful-api/model/web"
+	"github.com/raviMukti/gin-restful-api/repository"
+	"github.com/raviMukti/gin-restful-api/service"
 )
 
-func NewRoute(carController controller.CarController) *gin.Engine {
+func Init() {
+	err := godotenv.Load()
+	helper.PanicIfError(err)
+}
+
+var (
+	db            *sql.DB                  = InitDatabase()
+	carRepository repository.CarRepository = repository.NewCarRepository()
+	validate      *validator.Validate      = validator.New()
+	carService    service.CarService       = service.NewCarService(carRepository, db, validate)
+	carController controller.CarController = controller.NewCarController(carService)
+)
+
+func SetupRouter() *gin.Engine {
 
 	router := gin.Default()
+	router.HandleMethodNotAllowed = true
 	// Register Panic Handler
 	router.Use(gin.CustomRecovery(exception.ErrorHandler))
+	// Register No Route Handler
+	router.NoRoute(noRoutetoHost)
+	// Register Method Not Allowed Handler
+	router.NoMethod(noMethodAllowed)
 
-	// Register Routes
-	router.GET("/api/cars", carController.GetAll)
-	router.GET("/api/cars/:carId", carController.GetById)
-	router.POST("/api/cars", carController.Create)
-	router.PUT("/api/cars/:carId", carController.Update)
-	router.DELETE("/api/cars/:carId", carController.Remove)
+	// Register Car Routes Group
+	cars := router.Group("/api/cars")
+	{
+		cars.GET("/", carController.GetAll)
+		cars.GET("/:carId", carController.GetById)
+		cars.POST("/", carController.Create)
+		cars.PUT("/:carId", carController.Update)
+		cars.DELETE("/:carId", carController.Remove)
+	}
 
 	return router
+}
+
+func noRoutetoHost(ctx *gin.Context) {
+	ctx.Writer.Header().Add("Content-Type", "application/json")
+	ctx.Writer.WriteHeader(http.StatusNotFound)
+
+	webResponse := web.WebResponse{
+		Error:       "API_ERROR",
+		ErrorDetail: ctx.Request.URL.Path + " NOT FOUND",
+		Code:        http.StatusNotFound,
+		Message:     http.StatusText(http.StatusNotFound),
+		Data:        "",
+	}
+
+	ctx.JSON(http.StatusNotFound, webResponse)
+}
+
+func noMethodAllowed(ctx *gin.Context) {
+	ctx.Writer.Header().Add("Content-Type", "application/json")
+	ctx.Writer.WriteHeader(http.StatusMethodNotAllowed)
+
+	webResponse := web.WebResponse{
+		Error:       "API_ERROR",
+		ErrorDetail: ctx.Request.Method + " NOT ALLOWED",
+		Code:        http.StatusMethodNotAllowed,
+		Message:     http.StatusText(http.StatusMethodNotAllowed),
+		Data:        "",
+	}
+
+	ctx.JSON(http.StatusMethodNotAllowed, webResponse)
 }
--- main.go
+++ main.go
@@ -4,28 +4,14 @@ import (
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
-
-	"github.com/go-playground/validator"
-	"github.com/joho/godotenv"
 	"github.com/raviMukti/gin-restful-api/app"
-	"github.com/raviMukti/gin-restful-api/controller"
-	"github.com/raviMukti/gin-restful-api/helper"
-	"github.com/raviMukti/gin-restful-api/repository"
-	"github.com/raviMukti/gin-restful-api/service"
 )
 
 func main() {
 
-	err := godotenv.Load()
-	helper.PanicIfError(err)
+	app.Init()
 
-	db := app.NewDB()
-	validate := validator.New()
-	carRepository := repository.NewCarRepository()
-	carService := service.NewCarService(carRepository, db, validate)
-	carController := controller.NewCarController(carService)
-
-	router := app.NewRoute(carController)
+	router := app.SetupRouter()
 
 	router.Run(":" + os.Getenv("APP_PORT"))
 }
